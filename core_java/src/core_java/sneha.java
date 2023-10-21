2932f4dd40be737c9bce8636f31097809883e8d3
Acer <Acer@DESKTOP-EE08AGG>
2023-10-21 19:22:12 +0530

    my project

---
+++ core_java/src/core_java/sneha.java
@@ -0,0 +1,12 @@
+package core_java;
+
+public class sneha {
+
+	public static void main(String[] args) {
+		int n1=5,n2=4,sum;
+		sum=n1+n2;
+		System.out.println("Sum of numbers is:"+sum);
+
+	}
+
+}
